fc6362a0348103ad669efc280acdfdddc17ef2b4
SzXPeter <szabo.peter@students.jedlik.eu>
2025-04-04 08:49:34 +0200

    Refactor collision detection logic; standardize property naming and encapsulate collision checks into separate methods

--- Character.js
+++ Character.js
@@ -5,7 +5,7 @@ export default class Character extends CanvasElement {
         super(canvas, ctx);
         this.Speed = speed;
         this.Health = health;
-        this.collisionRadius = collisionRadius;
+        this.CollisionRadius = collisionRadius;
     }
 
     SpawnCharacter(x, y) {
@@ -21,48 +21,43 @@ export default class Character extends CanvasElement {
         }
         this.Clear();
     
-        const nextX = this.x + this.moveX * (deltaTime / 1000);
-        const nextY = this.y + this.moveY * (deltaTime / 1000);
+        const NextX = this.x + this.MoveX * (deltaTime / 1000);
+        const NextY = this.y + this.MoveY * (deltaTime / 1000);
     
-        let canMoveX = true;
-        let canMoveY = true;
-    
-        if (room.IsCollidingWithWall(nextX, this.y, this.collisionRadius)) {
-            canMoveX = false;
-        }
-    
-        if (room.IsCollidingWithWall(this.x, nextY, this.collisionRadius)) {
-            canMoveY = false;
-        }
-    
-        for (const character of otherCharacters) {
-            if (character !== this && this.IsCollidingWithCharacter(nextX, this.y, character)) {
-                canMoveX = false;
-            }
-        }
-    
-        for (const character of otherCharacters) {
-            if (character !== this && this.IsCollidingWithCharacter(this.x, nextY, character)) {
-                canMoveY = false;
-            }
-        }
-    
-        if (canMoveX) {
-            this.x = nextX;
-        }
-        if (canMoveY) {
-            this.y = nextY;
-        }
+        if (!this.IsCollidingHorizontally(room, NextX, otherCharacters))
+            this.x = NextX;
+        if (!this.IsCollidingVertically(room, NextY, otherCharacters))
+            this.y = NextY;
     
         this.Rotate = this.CalculateRotation();
         this.Draw();
         return false;
     }
+
+    IsCollidingHorizontally(room, nextX, otherCharacters) {
+        if (room.IsCollidingWithWall(nextX, this.y, this.CollisionRadius))
+            return true;
+
+        for (const character of otherCharacters)
+            if (character !== this && this.IsCollidingWithCharacter(nextX, this.y, character))
+                return true;
+        return false;
+    }
+
+    IsCollidingVertically(room, nextY, otherCharacters) {
+        if (room.IsCollidingWithWall(this.x, nextY, this.CollisionRadius))
+            return true;
+
+        for (const character of otherCharacters)
+            if (character !== this && this.IsCollidingWithCharacter(this.x, nextY, character))
+                return true;
+        return false;
+    }
     
     CalculateTargetAngle(targetX, targetY) {
         this.angleToPoint = Math.atan2(targetY - this.y, targetX - this.x);
-        this.moveX = Math.cos(this.angleToPoint) * this.Speed;
-        this.moveY = Math.sin(this.angleToPoint) * this.Speed;
+        this.MoveX = Math.cos(this.angleToPoint) * this.Speed;
+        this.MoveY = Math.sin(this.angleToPoint) * this.Speed;
     }
 
     CalculateRotation() {
@@ -73,6 +68,6 @@ export default class Character extends CanvasElement {
         const dx = nextX - otherCharacter.x;
         const dy = nextY - otherCharacter.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
-        return distance < otherCharacter.collisionRadius + this.collisionRadius;
+        return distance < (otherCharacter.CollisionRadius + this.CollisionRadius);
     }
 }
