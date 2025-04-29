566d18e56bd43e596fbf0727f57bb84ff9d4ece3
SzexPéter <szabo.peter@students.jedlik.eu>
2025-04-29 18:29:15 +0200

    #4 Implement shooting mechanics in Player class; add bullet spawning and firing logic

--- Player.js
+++ Player.js
@@ -1,4 +1,5 @@
 import Character from "./Character.js";
+import Bullet from "./Bullet.js";
 
 export default class Player extends Character {
     constructor(canvas, ctx, speed = 1, health = 100) {
@@ -11,6 +12,62 @@ export default class Player extends Character {
             this.Clientx = event.clientX;
             this.Clienty = event.clientY;
         });
+
+        this.Bullets = [];
+        this.canShoot = true;
+        this.fireRate = 100;
+        this.bulletDamage = 15;
+        this.bulletSpeed = 1000;
+
+        window.addEventListener('mousedown', (event) => {
+            if (event.button === 0) {
+                this.startShooting();
+            }
+        });
+
+        window.addEventListener('mouseup', (event) => {
+            if (event.button === 0) {
+                this.stopShooting();
+            }
+        });
+
+        this.shootingInterval = null;
+    }
+
+    startShooting() {
+        if (this.shootingInterval) return;
+        
+        const shoot = () => {
+            if (this.canShoot) {
+                this.shoot();
+                this.canShoot = false;
+                setTimeout(() => this.canShoot = true, this.fireRate);
+            }
+        };
+
+        shoot();
+        this.shootingInterval = setInterval(shoot, this.fireRate);
+    }
+
+    stopShooting() {
+        if (this.shootingInterval) {
+            clearInterval(this.shootingInterval);
+            this.shootingInterval = null;
+        }
+    }
+
+    shoot() {
+        const bullet = new Bullet(this.Canvas, this.ctx, this.bulletDamage, this.bulletSpeed);
+        const angle = Math.atan2(this.Clienty - this.Canvas.getBoundingClientRect().top - this.y, 
+                               this.Clientx - this.Canvas.getBoundingClientRect().left - this.x);
+        
+        const spawnX = this.x + Math.cos(angle) * (this.CollisionRadius + 10);
+        const spawnY = this.y + Math.sin(angle) * (this.CollisionRadius + 10);
+        
+        bullet.SpawnCharacter(spawnX, spawnY);
+        bullet.SetDirection(angle);
+        
+        this.Bullets.push(bullet);
     }
 
     MovePlayer(deltaTime = 16.66667, room, otherCharacters = []) {
@@ -32,6 +89,10 @@ export default class Player extends Character {
         this.MoveTowardsPoint(x, y, deltaTime, room, otherCharacters);
     }
 
+    GetBullets() {
+        return this.Bullets;
+    }
+
     DrawCharacter() {
         this.ctx.fillStyle = "purple";
         this.ctx.beginPath();
@@ -39,15 +100,19 @@ export default class Player extends Character {
         this.ctx.fill();
         this.ctx.closePath();
 
-        const grad = this.ctx.createLinearGradient(-20, 0, 20, 0);
+        const grad = this.ctx.createLinearGradient(-20, 0, 25, 0);
         
+        const healthPercentage = this.CurrentHealth / this.Health;
         grad.addColorStop(0, "green");
-        grad.addColorStop(this.CurrentHealth / this.Health, "green");
-        grad.addColorStop(this.CurrentHealth / this.Health, "red");
+        grad.addColorStop(healthPercentage < 0 ? 0 : healthPercentage, "green");
+        grad.addColorStop(healthPercentage < 0 ? 0 : healthPercentage, "red");
         grad.addColorStop(1, "red");
 
+        this.ctx.fillStyle = "black";
+        this.ctx.fillRect(-5, -this.CollisionRadius + 2, 10, 20);
+
         this.ctx.fillStyle = grad;
-        this.ctx.fillRect(-20, -20, 40, 10);
+        this.ctx.fillRect(-25, -10, 50, 10);
     }
 
     CalculateRotation() {
